b7e2ebd68984d192c432998f44cb252a00ee3330
Santiago Villa Fernandez <svillafernandez@gmail.com>
2013-07-25 20:14:23 -0300

    First project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnElSillon::Application.config.secret_key_base = '89c03e6b11985d4f18f031e99e5d7cdef74d6bb0d48fafbc774604037cda63a6eb169b1adedb8cd887c28d59fbf09247fc8ee9218172c8ac25946e08014a5fce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnElSillon::Application.config.session_store :cookie_store, key: '_en-el-sillon_session'
