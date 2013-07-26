b213fc226fad808df7add1a09a0e9771ffaa6006
Santiago Villa Fernandez <svillafernandez@gmail.com>
2013-07-26 01:12:03 -0300

    Added the static controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 EnElSillon::Application.routes.draw do
+  
+  root :to => 'static#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
